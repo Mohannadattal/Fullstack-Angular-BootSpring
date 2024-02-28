40b4a0b6033892a4d021b9a8980450fd942a57b5
Mohannadgithub <mohannadattal85@gmail.com>
2024-02-28 12:47:45 +0100

    enhanced menu component to read data from product service and updated product service to call URL on backend

--- 03-frontend/ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
+++ 03-frontend/ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -5,28 +5,21 @@ import { ProductService } from '../../services/product.service';
 @Component({
   selector: 'app-product-category-menu',
   templateUrl: './product-category-menu.component.html',
-  styleUrl: './product-category-menu.component.css'
+  styleUrl: './product-category-menu.component.css',
 })
 export class ProductCategoryMenuComponent implements OnInit {
-
   productCategories: ProductCategory[] = [];
 
-  constructor( private productService: ProductService) {}
-
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.listProductCategories()
+    this.listProductCategories();
   }
-
 
   listProductCategories() {
-  this.productService.getProductCategory().subscribe(
-    data => {
+    this.productService.getProductCategory().subscribe((data) => {
       console.log('Product Categories =' + JSON.stringify(data));
       this.productCategories = data;
-
-    }
-  )
+    });
   }
-
 }
