94dde5ee1dc7521d2524cd49df49c51f6964b1f9
Mohannadgithub <mohannadattal85@gmail.com>
2024-02-28 15:28:07 +0100

    Created new component for search and added new route for searching und updated search component to send data to search route

--- 03-frontend/ecommerce/src/app/app.module.ts
+++ 03-frontend/ecommerce/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
+  { path: 'search/:keyword', component: ProductListComponent },
   { path: 'category/:id/:name', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
@@ -18,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent],
+  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
