754bd6359b73388aac67a5fcad42a680ca778a3d
Mohannadgithub <mohannadattal85@gmail.com>
2024-03-01 13:04:21 +0100

    Added pagination support to product service

--- 03-frontend/ecommerce/src/app/services/product.service.ts
+++ 03-frontend/ecommerce/src/app/services/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getProduct(productId: number): Observable<Product> {
     // build URL based on product id
@@ -21,12 +21,17 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
-  getProductListPaginate(page: number, pageSize: number, categoryId: number): Observable<GetResponseProducts> {
+  getProductListPaginate(
+    page: number,
+    pageSize: number,
+    categoryId: number
+  ): Observable<GetResponseProducts> {
     // build URL based on category id, page and size
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}` + `&page=${page}&size=${pageSize}`;
+    const searchUrl =
+      `${this.baseUrl}/search/findByCategoryId?id=${categoryId}` +
+      `&page=${page}&size=${pageSize}`;
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
-
 
   getProductList(categoryId: number): Observable<Product[]> {
     // build URL based on category id
@@ -38,6 +43,18 @@ export class ProductService {
     // build URL based on the keyword
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
     return this.getProducts(searchUrl);
+  }
+
+  searchProductPaginate(
+    page: number,
+    pageSize: number,
+    theKeyword: string
+  ): Observable<GetResponseProducts> {
+    // build URL based on keyword, page and size
+    const searchUrl =
+      `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}` +
+      `&page=${page}&size=${pageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
