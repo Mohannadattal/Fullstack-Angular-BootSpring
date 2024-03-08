c16f755001b252bc187c269bb8589d44e8a76536
Mohannadgithub <mohannadattal85@gmail.com>
2024-03-08 15:29:58 +0100

    created entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mocode/springbootecommerce/entity/Address.java
@@ -0,0 +1,31 @@
+package com.mocode.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "address")
+@Data
+
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+}
---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mocode/springbootecommerce/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.mocode.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
