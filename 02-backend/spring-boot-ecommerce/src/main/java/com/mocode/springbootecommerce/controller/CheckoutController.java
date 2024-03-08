3914116ab83f64f62201d54fec70099baffe991b
Mohannadgithub <mohannadattal85@gmail.com>
2024-03-08 16:48:30 +0100

    Created controller

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mocode/springbootecommerce/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package com.mocode.springbootecommerce.controller;
+
+import com.mocode.springbootecommerce.dto.Purchase;
+import com.mocode.springbootecommerce.dto.PurchaseResponse;
+import com.mocode.springbootecommerce.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+        return purchaseResponse;
+    }
+}
