7e1d7a16b44244f4878c6f3206492d582656a815
Mohannadgithub <mohannadattal85@gmail.com>
2024-03-14 12:11:46 +0100

    Added openID Connect client app in okta and setup app configuration for openID connect

---
+++ 03-frontend/ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oafs16h40q2RG2LM5d7',
+    issuer: 'https://dev-94744868.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
