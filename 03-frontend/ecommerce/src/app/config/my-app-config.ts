9871a4d25fc9262bceea498323d142029054cba3
Mohannadgithub <mohannadattal85@gmail.com>
2024-03-21 11:06:23 +0100

    updated Redirect Url

--- 03-frontend/ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oafs16h40q2RG2LM5d7',
     issuer: 'https://dev-94744868.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
